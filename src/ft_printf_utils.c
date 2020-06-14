ba81efbe7d33d5430d62c1f0170c1259d2a77191
awerebea <awerebea.21@gmail.com>
2020-06-14 12:51:15 +0300

    f_isspec corrected

--- src/ft_printf_utils.c
+++ src/ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/28 00:25:59 by awerebea          #+#    #+#             */
-/*   Updated: 2020/06/07 23:50:14 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/06/14 12:50:21 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,29 +67,29 @@ wchar_t			*f_wstrdup(const wchar_t *ws)
 
 char			f_isspec(const char *format, int *i, t_opts *opts)
 {
-	char	*sp;
-	int		j;
-
-	j = *i;
-	if (!format[j])
+	if (!format[*i])
 		return (0);
-	if (format[j] == 'l' || format[j] == 'L')
+	if (format[*i] == 'L')
 	{
-		opts->subspec = (format[j] == 'l') ? 8 : 4;
-		j++;
-		if (format[j++] == 'l' && !((opts->subspec) & 4))
+		opts->subspec += 4;
+		(*i)++;
+	}
+	else if (format[*i] == 'l')
+	{
+		opts->subspec = 8;
+		(*i)++;
+		if (format[(*i)++] == 'l')
 			opts->subspec += 4;
-		j -= (opts->subspec & 4) ? 0 : 1;
+		*i -= (opts->subspec & 4) ? 0 : 1;
 	}
-	else if (format[j] == 'h')
+	else if (format[*i] == 'h')
 	{
-		j++;
+		(*i)++;
 		opts->subspec = 2;
-		if (format[j++] == 'h')
+		if (format[(*i)++] == 'h')
 			opts->subspec += 1;
-		j -= (opts->subspec & 1) ? 0 : 1;
+		*i -= (opts->subspec & 1) ? 0 : 1;
 	}
-	*i = j;
-	sp = ft_printf_strchr("cCspdiuoxXnfFgGeE%", format[*i]);
-	return (sp) ? *sp : 0;
+	return (ft_printf_strchr("cCspdiuoxXnfFgGeE%", format[*i])) ? \
+	format[*i] : 0;
 }
